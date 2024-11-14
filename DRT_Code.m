d21ce4d65a20a02a353b5ba16f22109add6275f8
yjung <yjung@kentech.ac.kr>
2024-11-14 14:36:43 +0900

    DRT_code.m

--- DRT_Code.m
+++ DRT_Code.m
@@ -9,59 +9,59 @@ data = loaded_struct.AS2_1per; % 파일이 바뀌면 바꿔야함
 %% 미리 할당할 총 조합 수 계산
 num_combinations = 0;
 
-% 조합 1: dt=0.1, duration=1000, N=[201, 101, 21]
+% 조합 1: dt=0.1, duration=1000, n=[201, 101, 21]
 num_combinations = num_combinations + length([201, 101, 21]);
 
-% 조합 2: dt=[0.2, 1, 2], duration=1000, N=201
+% 조합 2: dt=[0.2, 1, 2], duration=1000, n=201
 num_combinations = num_combinations + length([0.2, 1, 2]);
 
-% 조합 3: dt=0.1, duration=[500, 250], N=201
+% 조합 3: dt=0.1, duration=[500, 250], n=201
 num_combinations = num_combinations + length([500, 250]);
 
 % combinations 구조체 배열 미리 할당
-combinations(num_combinations) = struct('dt', [], 'duration', [], 'N', []);
+combinations(num_combinations) = struct('dt', [], 'duration', [], 'n', []);
 
 idx = 1;
 
 % 조합 1
 dt = 0.1;
 duration = 1000;
-N_list = [201, 101, 21];
-for i = 1:length(N_list)
-    N = N_list(i);
+n_list = [201, 101, 21];
+for i = 1:length(n_list)
+    n = n_list(i);
     combinations(idx).dt = dt;
     combinations(idx).duration = duration;
-    combinations(idx).N = N;
+    combinations(idx).n = n;
     idx = idx + 1;
 end
 
 % 조합 2
 dt_list = [0.2, 1, 2];
 duration = 1000;
-N = 201;
+n = 201;
 for i = 1:length(dt_list)
     dt = dt_list(i);
     combinations(idx).dt = dt;
     combinations(idx).duration = duration;
-    combinations(idx).N = N;
+    combinations(idx).n = n;
     idx = idx + 1;
 end
 
 % 조합 3
 dt = 0.1;
 duration_list = [500, 250];
-N = 201;
+n = 201;
 for i = 1:length(duration_list)
     duration = duration_list(i);
     combinations(idx).dt = dt;
     combinations(idx).duration = duration;
-    combinations(idx).N = N;
+    combinations(idx).n = n;
     idx = idx + 1;
 end
 
 % 결과를 저장할 struct 배열 초기화
 total_results = num_combinations * 10;
-AS2_1per_new(total_results) = struct('dt', [], 'dur', [], 'N', [], 'V', [], 'I', [], 't', [], 'V_est', []);
+AS2_1per_new(total_results) = struct('dt', [], 'dur', [], 'n', [], 'SN', [], 'V', [], 'I', [], 't', []);
 
 index = 1;
 
@@ -69,37 +69,35 @@ index = 1;
 for i = 1:length(combinations)
     dt = combinations(i).dt;
     duration = combinations(i).duration;
-    N = combinations(i).N;
+    n = combinations(i).n;
 
     for j = 1:10
         % 원본 데이터 가져오기
+        SN = data(j).SN;
         V_orig = data(j).V;
         I_orig = data(j).I;
         t_orig = data(j).t;
-        V_est_orig = data(j).V_est;
 
         % duration에 따른 데이터 리샘플링
         dur = t_orig <= duration;
         V_dur = V_orig(dur);
         I_dur = I_orig(dur);
         t_dur = t_orig(dur);
-        V_est_dur = V_est_orig(dur);
 
         % dt에 따른 데이터 리샘플링
         step = round(dt/0.1);
         V_new = V_dur(1:step:end);
         I_new = I_dur(1:step:end);
         t_new = t_dur(1:step:end);
-        V_est_new = V_est_dur(1:step:end);
-
+        
         % 결과 저장
+        AS2_1per_new(index).SN = SN;
         AS2_1per_new(index).dt = dt;
         AS2_1per_new(index).dur = duration;
-        AS2_1per_new(index).N = N;
+        AS2_1per_new(index).n = n;
         AS2_1per_new(index).V = V_new;
         AS2_1per_new(index).I = I_new;
         AS2_1per_new(index).t = t_new;
-        AS2_1per_new(index).V_est = V_est_new;
         index = index + 1;
     end
 end
